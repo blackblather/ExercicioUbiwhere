1fa1b1f9fc5fe2bff7c9d6024deba145e95ebf22
joaom <joao.monteiro95@hotmail.com>
2020-10-05 17:47:02 +0100

    Replaced manual search/404 exception by django shortcut

--- API/views.py
+++ API/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
@@ -24,13 +25,11 @@ class OcorrenciasUpdate(generics.UpdateAPIView):
     def partial_update(self, request, *args, **kwargs):
         try:
             self.__validate_request_format(request)
-            ocorrencia = self.queryset.get(pk=request.data["id"])
+            ocorrencia = get_object_or_404(self.queryset, pk=request.data["id"])
             serializer = self.serializer_class(ocorrencia, data={"estado": 2}, partial=True)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except Ocorrencias.DoesNotExist:
-            return Response("The specified ocorrencia does not exist", status=status.HTTP_404_NOT_FOUND)
         except ValueError as error:
             return Response(error.__str__(), status=status.HTTP_400_BAD_REQUEST)
