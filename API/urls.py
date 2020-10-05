e68a7f2d63004974950bef13860d1cae23e5443d
joaom <joao.monteiro95@hotmail.com>
2020-10-05 12:43:44 +0100

    Added default "obtain_auth_token" view to provide user authentication tokens

--- API/urls.py
+++ API/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken import views as auth_views
 
 urlpatterns = [
     path('', views.OcorrenciasListCreate.as_view()),
+    path('api-token-auth/', auth_views.obtain_auth_token)
 ]
