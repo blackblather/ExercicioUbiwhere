aa698c570e0b7a204f011f80df30f830f58439b2
joaom <joao.monteiro95@hotmail.com>
2020-10-05 10:36:07 +0100

    Added custom validation in OcorrenciasSerializer.create(...) to make sure estado always gets the default value ONLY during creation

--- API/serializers.py
+++ API/serializers.py
@@ -6,6 +6,11 @@ class OcorrenciasSerializer(serializers.ModelSerializer):
     data_criacao = serializers.DateField()
     data_atualizacao = serializers.DateField(required=False)
 
+    def create(self, validated_data):
+        if 'estado' in validated_data.keys():
+            validated_data.pop('estado')
+        return Ocorrencias.objects.create(**validated_data)
+
     class Meta:
         model = Ocorrencias
         fields = ['id', 'descricao', 'lat', 'lon', 'data_criacao', 'data_atualizacao', 'autor', 'estado', 'categoria']
